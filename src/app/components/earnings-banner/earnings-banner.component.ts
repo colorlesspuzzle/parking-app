e959b7af543dff3173957f15157e8183f856924a
Andres Serrano <andresfserrano@gmail.com>
2025-03-13 18:18:39 -0500

    4th commit

--- src/app/components/earnings-banner/earnings-banner.component.ts
+++ src/app/components/earnings-banner/earnings-banner.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ParkingLogicService } from '../../services/parking-logic.service';
 
 @Component({
   selector: 'app-earnings-banner',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './earnings-banner.component.html',
 })
+
 export class EarningsBannerComponent {
   constructor(private parkingLogic: ParkingLogicService) {}
 
